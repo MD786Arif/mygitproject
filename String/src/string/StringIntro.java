decc4aa09808a17a493f39f11fe790f24d9be332
MD786Arif <75136826+MD786Arif@users.noreply.github.com>
2020-12-07 22:30:45 +0530

    Test commit

---
+++ String/src/string/StringIntro.java
@@ -0,0 +1,83 @@
+package string;
+import java.util.Scanner;
+public class StringIntro {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		String animal1 = "dog";
+//		String animal2 = "";
+//	      String animal = new String("dog"); 
+		
+//		System.out.println(animal1==animal2);
+		
+//		String name = " MOHAMMAD ARIF ";
+//		name += "Kumar";
+//		System.out.println(name.replace('a','b'));
+		
+//		String Cars = "maruti,swift,hyundai,mercedes,ferari";
+//		String allCars[] = Cars.split(",");
+//		for(String car:allCars) {
+//			System.out.println(car);
+//		}
+//		System.out.println(Cars.indexOf('z'));
+		
+//		String name = "   arif   ";
+//		System.out.println(  name.trim()+name.toUpperCase());
+//		String Name = name.trim();
+//		System.out.println(Name.toUpperCase());
+		
+//		         Functions in String
+//		1
+//		String name = "   Arif";
+//		String name2 = new String("");
+//		System.out.println(name.charAt(8));
+		
+//		2
+//		System.out.println(name.length());
+		
+//		3
+//		System.out.println(name.substring(9));
+		
+//		4
+//                System.out.println(name.substring(0,9));		
+//		5
+//		System.out.println(name.contains("Arif"));
+//		6
+		
+//		System.out.println(name.equals(name2));
+//		7
+//		System.out.println(name2.isEmpty());
+		
+//		8
+//		System.out.println(name.concat("Khan"));
+		
+//		9
+//		System.out.println(name.replace('r', 's'));
+		
+//		10
+		
+//		String cars = "Maruti,Suzuki,Mercedes,Hyundai,mahindra";
+//		String allCars[] = cars.split(",");
+//		for(String car:allCars) {
+//			System.out.println(car);
+//		}
+//		System.out.println(cars.toUpperCase());
+//		System.out.println(cars.toLowerCase());
+		
+//		System.out.println(name.trim());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
